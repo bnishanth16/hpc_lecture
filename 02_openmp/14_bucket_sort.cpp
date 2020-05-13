9e8fb01503f2da9cdbc9600074b55fa0acbda22e
bnishanth16 <65318177+bnishanth16@users.noreply.github.com>
2020-05-14 08:48:48 +0900

    Update 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,3 +1,6 @@
+// Name: Nishanth Baskaran
+// Student ID: 19M15017
+// HPSC Assignment-L2 
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
@@ -13,19 +16,32 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range); 
+  
+  #pragma omp parallel for //new code 
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
+ 
+ #pragma omp parallel //new code
+ #pragma omp sections firstprivate(bucket) //new code 
+ {
+   #pragma omp section //new code
   for (int i=0; i<n; i++) {
     bucket[key[i]]++;
   }
-  for (int i=0, j=0; i<range; i++) {
+ 
+  #pragma omp section //new code
+  for (int i=0, j=0; i<range; i++) { 
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
+ }
+  
 
+  #pragma omp parallel //new code
   for (int i=0; i<n; i++) {
+    #pragma omp single // new code
     printf("%d ",key[i]);
   }
   printf("\n");
