a67b34cce08860cb5bfadee3db2f8a24ba1d234b
bnishanth16 <65318177+bnishanth16@users.noreply.github.com>
2020-05-14 08:46:42 +0900

    Update 12_pi.cpp

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,9 +1,13 @@
+// Name: Nishanth Baskaran
+// Student ID: 19M15017
+// HPSC Assignment-L2 
 #include <cstdio>
 
 int main() {
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
+  #pragma omp parallel for reduction(+:pi) //new code added
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
