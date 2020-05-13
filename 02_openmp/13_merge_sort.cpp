bb88bcf8d1b8ec664bc31b63e910d143f2737d36
bnishanth16 <65318177+bnishanth16@users.noreply.github.com>
2020-05-14 08:48:08 +0900

    Update 13_merge_sort.cpp

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -1,3 +1,6 @@
+// Name: Nishanth Baskaran
+// Student ID: 19M15017
+// HPSC Assignment-L2 
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
@@ -7,7 +10,8 @@ void merge(std::vector<T>& vec, int begin, int mid, int end) {
   std::vector<T> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+  
+    for (int i=0; i<tmp.size(); i++) {
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -15,18 +19,26 @@ void merge(std::vector<T>& vec, int begin, int mid, int end) {
     else if (vec[left] <= vec[right])
       tmp[i] = vec[left++];
     else
-      tmp[i] = vec[right++]; 
+      tmp[i] = vec[right++];     
   }
-  for (int i=0; i<tmp.size(); i++) 
+  
+  for (int i=0; i<tmp.size(); i++) {
     vec[begin++] = tmp[i];
+  }
 }
 
 template<class T>
 void merge_sort(std::vector<T>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+
+    #pragma omp task shared(vec) //New code
     merge_sort(vec, begin, mid);
+
+    #pragma omp task shared(vec) //New code
     merge_sort(vec, mid+1, end);
+
+    #pragma omp taskwait //New code
     merge(vec, begin, mid, end);
   }
 }
@@ -42,7 +54,9 @@ int main() {
 
   merge_sort(vec, 0, n-1);
 
+  #pragma omp parallel //New code
   for (int i=0; i<n; i++) {
+    #pragma omp single //New code
     printf("%d ",vec[i]);
   }
   printf("\n");
