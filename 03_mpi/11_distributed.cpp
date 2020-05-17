a564948f1a29abc6d0d749f6031859fcf97d898e
bnishanth16 <65318177+bnishanth16@users.noreply.github.com>
2020-05-18 08:41:06 +0900

    Assignment completed

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -1,3 +1,6 @@
+// Name: Nishanth Baskaran
+// Student ID: 19M15017
+// HPSC Assignment-L3 
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
@@ -26,9 +29,20 @@ int main(int argc, char** argv) {
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+
+  // since there is a collision between read and write create new memory for recv
+  Body recv_jbody[N/size];
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
+    // MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    MPI_Win win; //New code
+    MPI_Win_create(recv_jbody, (N/size)*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win); //New code
+    MPI_Win_fence(0, win);//New code
+    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);//New code
+    MPI_Win_fence(0, win);//New code
+    for (int k = 0; k < N/size; k++) jbody[k]=recv_jbody[k]; //New code (copy back recv_body to jbody)
+      
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
@@ -40,6 +54,7 @@ int main(int argc, char** argv) {
         }
       }
     }
+    MPI_Win_free(&win); // new code
   }
   for(int irank=0; irank<size; irank++) {
     MPI_Barrier(MPI_COMM_WORLD);
@@ -49,5 +64,6 @@ int main(int argc, char** argv) {
       }
     }
   }
+
   MPI_Finalize();
 }
